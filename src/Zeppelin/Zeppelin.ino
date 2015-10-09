03552d1530d3679ddfacda62fb38b50613810e20
Vinicius Tavares <vinerz@vinerz.net>
2015-10-09 14:26:48 -0300

    cleanup and organization stuff

--- src/Zeppelin/Zeppelin.ino
+++ src/Zeppelin/Zeppelin.ino
@@ -15,19 +15,6 @@
 #define ANIM_TIMEINIT 4
 #define ANIM_DURATION 5
 
-String propl_labels[3] = { "Back_Left", "Back_Right", "Down" };
-int PROP[3] = { A0, A1, A2 };
-
-int SENS[4][2] = { {2,3}, {4,5}, {6,7}, {8,9} }; // Sensors Ports
-const int sens_rounds = 3; // rounds to consider a valid distance from sensors
-
-String sens_labels[4] = { "Left", "Right", "Front", "Down" }; // human readable labels for sensors
-int sensReadCount[4] = { 0, 0, 0, 0 }; // read count for each sensor
-int currentAverages[4] = { -1, -1, -1, -1 }; // valid averages for zeppelin usage
-int latestAverages[4] = { -1, -1, -1, -1 }; // current unofficial averages that will be put on valid averages after x rounds
-int latestDistances[4] = { -1, -1, -1, -1 }; // latest distance gotten from sensors
-
-
 void setup() {
   Serial.begin(9600);
   
@@ -62,13 +49,17 @@ void loop() {
   
   Serial.println("-== END OF REPORT ==-");*/
   
-  delay(20);
+  delay(1);
 }
 
-void prepareSensor(int sensor) {
-  pinMode(SENS[sensor][0], OUTPUT);
-  pinMode(SENS[sensor][1], INPUT);
-}
+/*
+ * -=============================-
+ *  PROPELLER ENGINEERING STUFF 
+ * -=============================-
+ */
+
+String prop_labels[3] = { "Back_Left", "Back_Right", "Down" };
+int PROP[3] = { A0, A1, A2 };
 
 void preparePropeller(int propeller) {
   pinMode(PROP[propeller], OUTPUT);
@@ -93,6 +84,9 @@ void fullSpeedPropeller(int propeller, int duration) {
   setPropellerSpeed(propeller, 255);
 }
 
+/* PROPELLER ANIMATIONS */
+/* This methods were created for smooth acceleration and breaking of the propellers, due to the Zeppeling long momentum. */
+
 long PROPL_ANIMATIONS[3][6] = {
   { 0, 0, 0, 0, 0, 0 },
   { 0, 0, 0, 0, 0, 0 },
@@ -100,9 +94,20 @@ long PROPL_ANIMATIONS[3][6] = {
 };
 
 void resetPropellerAnimation(int propeller) {
+  PROPL_ANIMATIONS[propeller][0] = 0;
+  PROPL_ANIMATIONS[propeller][1] = 0;
+  PROPL_ANIMATIONS[propeller][2] = 0;
+  PROPL_ANIMATIONS[propeller][3] = 0;
+  PROPL_ANIMATIONS[propeller][4] = 0;
+  PROPL_ANIMATIONS[propeller][5] = 0;
 }
 
 void startPropellerAnimation(int propeller, int objective, int duration) {
+  resetPropellerAnimation(propeller); // clear previous animation garbage
+  
+  PROPL_ANIMATIONS[propeller][ANIM_OBJECTIVE] = objective;
+  PROPL_ANIMATIONS[propeller][ANIM_DURATION] = duration;
+  PROPL_ANIMATIONS[propeller][ANIM_ENABLED] = 1;
 }
 
 void _propellerAnimationFrame(int propeller) {
@@ -125,9 +130,9 @@ void _propellerAnimationFrame(int propeller) {
       framesPerMs = (float) diffFrames / PROPL_ANIMATIONS[propeller][ANIM_DURATION]; // calculate frames per ms
     }
     
-    float shouldvePassed = framesPerMs * (float) diffTime;
+    float currentStage = framesPerMs * (float) diffTime;
     
-    PROPL_ANIMATIONS[propeller][ANIM_CURRENTVAL] = floor(initVal + shouldvePassed);
+    PROPL_ANIMATIONS[propeller][ANIM_CURRENTVAL] = floor(initVal + currentStage);
   
     if( (initVal < objective && PROPL_ANIMATIONS[propeller][ANIM_CURRENTVAL] >= objective)
         || (initVal > objective && PROPL_ANIMATIONS[propeller][ANIM_CURRENTVAL] <= objective) ) {
@@ -135,11 +140,31 @@ void _propellerAnimationFrame(int propeller) {
       PROPL_ANIMATIONS[propeller][ANIM_ENABLED] = 0;
     }
 
-    Serial.print("Shouldve Passed: "); Serial.println(shouldvePassed);
+    Serial.print("Current Speed for Propeller: "); Serial.println(currentStage);
     Serial.println(PROPL_ANIMATIONS[propeller][ANIM_CURRENTVAL]);
   } else {
-    Serial.println("No animation enabled for Propeller " + (propeller + 1));
+    Serial.println("No animation enabled for Propeller " + prop_labels[propeller]);
   }
+}
+
+/*
+ * -=========================-
+ *  ULTRASONIC SENSOR STUFF 
+ * -=========================-
+ */
+
+int SENS[4][2] = { {2,3}, {4,5}, {6,7}, {8,9} }; // Sensors Ports
+const int sens_rounds = 3; // rounds to consider a valid distance from sensors
+
+String sens_labels[4] = { "Left", "Right", "Front", "Down" }; // human readable labels for sensors
+int sensReadCount[4] = { 0, 0, 0, 0 }; // read count for each sensor
+int currentAverages[4] = { -1, -1, -1, -1 }; // valid averages for zeppelin usage
+int latestAverages[4] = { -1, -1, -1, -1 }; // current unofficial averages that will be put on valid averages after x rounds
+int latestDistances[4] = { -1, -1, -1, -1 }; // latest distance gotten from sensors
+
+void prepareSensor(int sensor) {
+  pinMode(SENS[sensor][0], OUTPUT);
+  pinMode(SENS[sensor][1], INPUT);
 }
 
 long extractAverage(int sensor) {
